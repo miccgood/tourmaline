2358fe54751b653fb2e8fee04516e1f19ef9aaac
miccgood <micverygood@hotmail.com>
2015-12-09 14:04:08 +0700

    init

---
+++ application/views/themes/tour.php
@@ -0,0 +1,136 @@
+<html lang="en">
+	<head>
+		<title><?php echo $title; ?></title>
+		<meta name="resource-type" content="document" />
+		<meta name="robots" content="all, index, follow"/>
+		<meta name="googlebot" content="all, index, follow" />
+	<?php
+	/** -- Copy from here -- */
+	if(!empty($meta))
+	foreach($meta as $name=>$content){
+		echo "\n\t\t";
+		?><meta name="<?php echo $name; ?>" content="<?php echo $content; ?>" /><?php
+			 }
+	echo "\n";
+
+	if(!empty($canonical))
+	{
+		echo "\n\t\t";
+		?><link rel="canonical" href="<?php echo $canonical?>" /><?php
+
+	}
+	echo "\n\t";
+
+	foreach($css as $file){
+	 	echo "\n\t\t";
+		?><link rel="stylesheet" href="<?php echo $file; ?>" type="text/css" /><?php
+	} echo "\n\t";
+
+	foreach($js as $file){
+			echo "\n\t\t";
+			?><script src="<?php echo $file; ?>"></script><?php
+	} echo "\n\t";
+
+	/** -- to here -- */
+?>
+    <link href="<?php echo base_url(); ?>assets/homepage/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+    <link href="<?php echo base_url(); ?>assets/homepage/css/shop-homepage.css" rel="stylesheet" type="text/css"/>
+    <!-- Le styles -->
+    
+<!--    <link href="<?php echo base_url(); ?>assets/themes/default/hero_files/bootstrap.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/themes/default/hero_files/bootstrap-responsive.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/themes/default/css/general.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/themes/default/css/custom.css" rel="stylesheet">-->
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Le fav and touch icons -->
+    
+    <!-- jQuery -->
+    
+    <script src="<?php echo base_url(); ?>assets/homepage/js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?php echo base_url(); ?>assets/homepage/js/bootstrap.min.js"></script>
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="<?php echo base_url() . "index.php/tourmaline/#";?>">
+                    <img src="<?php echo base_url("assets/themes/default/images/logo-01.png")?>" width="150px"/>
+                </a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Services</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-md-3">
+                <p class="lead">Shop Name</p>
+                <div class="list-group">
+                    <a href="#" class="list-group-item">Category 1</a>
+                    <a href="#" class="list-group-item">Category 2</a>
+                    <a href="#" class="list-group-item">Category 3</a>
+                </div>
+            </div>
+            
+            <div class="col-md-9">
+                <?php echo $output;?>
+            </div>
+
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+    <div class="container">
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Your Website 2014</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+
+</body>
+
+</html>
